4776052ab711bec3091db0e6637dd3fd72a676ab
Dellism <mostsity81@gmail.com>
2023-09-15 12:37:58 +0300

    Added AC compatibility to ElytraLimiter

--- src/main/java/me/vbaddon/modules/ElytraLimiter.java
+++ src/main/java/me/vbaddon/modules/ElytraLimiter.java
@@ -1,12 +1,16 @@
 package me.vbaddon.modules;
 
 import me.vbaddon.VBAddon;
-import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.events.entity.player.PlayerMoveEvent;
+import meteordevelopment.meteorclient.mixininterface.IVec3d;
+import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
+
+import java.util.Random;
 
 public class ElytraLimiter extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
@@ -18,25 +22,39 @@ public class ElytraLimiter extends Module {
         .sliderMax(40)
         .build()
     );
+
+    private final Setting<Boolean> ac = sgGeneral.add(new BoolSetting.Builder()
+        .name("ac-compatibility")
+        .description("Anticheat compatibility on karasique.")
+        .defaultValue(true)
+        .build()
+    );
+
     public ElytraLimiter() {
         super(VBAddon.CATEGORY, "elytra-limiter", "Limits elytra speed to some value in block per second.");
     }
 
-    /*@EventHandler
+    @EventHandler
     private void onPlayerMove(PlayerMoveEvent event) {
+        Random random = new Random();
         double maxspeed = maxSpeed.get();
         double speed = event.movement.horizontalLength() * 20;
         if (!mc.player.isFallFlying() || speed <= maxspeed) return;
-        double multiplier = maxspeed / speed;
+        double multiplier;
+        if (ac.get())
+            multiplier = maxspeed / speed + random.nextDouble() % 0.01 - 0.01;
+        else
+            multiplier = maxspeed / speed;
         ((IVec3d) event.movement).setXZ(event.movement.x * multiplier, event.movement.z * multiplier);
-    }*/
-    @EventHandler
+    }
+    /*@EventHandler
     private void onTick(TickEvent.Pre event) {
+
         double maxspeed = maxSpeed.get();
         double speed = mc.player.getVelocity().horizontalLength() * 20;
         //info("speed: " + speed);
         if (!mc.player.isFallFlying() || speed <= maxspeed) return;
         double multiplier = maxspeed / speed;
         mc.player.setVelocity(mc.player.getVelocity().x * multiplier, mc.player.getVelocity().y, mc.player.getVelocity().z * multiplier);
-    }
+    }*/
 }
