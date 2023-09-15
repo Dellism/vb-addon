985df960cf0224d921d1cc220acf547ee9d5a70c
Dellism <mostsity81@gmail.com>
2023-09-15 11:01:53 +0300

    Fixed miltiple elytras in inventory glitch, added only on firework mode.

--- src/main/java/me/vbaddon/modules/AutoChestSwap.java
+++ src/main/java/me/vbaddon/modules/AutoChestSwap.java
@@ -2,26 +2,41 @@ package me.vbaddon.modules;
 
 import me.vbaddon.VBAddon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
 public class AutoChestSwap extends Module {
     int ticksOnGround = 1;
+
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
     public AutoChestSwap() {
         super(VBAddon.CATEGORY, "auto-chest-swap", "Automatically switches between chestplate and elytra.");
     }
+
+    private final Setting<Boolean> firework = sgGeneral.add(new BoolSetting.Builder()
+        .name("only-when-holding-firework")
+        .description("You have to hold firework in hand to switch to elytra.")
+        .defaultValue(false)
+        .build()
+    );
 
     @EventHandler
     private void onTick(TickEvent.Post event) {
         Item currentItem = mc.player.getEquippedStack(EquipmentSlot.CHEST).getItem();
 
-        if (ticksOnGround == 0) {
-            equipElytra();
-        } else if (ticksOnGround == 20) {
+        if (ticksOnGround == 0 && currentItem != Items.ELYTRA) {
+            if (!firework.get()) equipElytra();
+            if (firework.get() && mc.player.getMainHandStack().getItem() == Items.FIREWORK_ROCKET)
+                equipElytra();
+        } else if (ticksOnGround == 20 && !(currentItem instanceof ArmorItem && ((ArmorItem) currentItem).getSlotType() == EquipmentSlot.CHEST)) { //unreadable
             equipChestplate();
         }
 
